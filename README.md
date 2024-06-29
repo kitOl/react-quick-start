daf499e70d7cd82d7d9d4f1dbfe798c14b1f2205
Oleg Kitaev <kitaev_oleg@mail.ru>
2024-06-29 15:30:01 +0300

    add http request and loader

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 ## How To
 
 `npm i props-types`
+
+## Links
+
+https://loading.io/css/
--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import TodoList from "./Todo/TodoList";
 import Context from "./context";
 import AddTodo from "./Todo/AddTodo";
+import Loader from "./Loader";
 
 function App() {
-  const [todos, setTodos] = useState([
-    { id: 1, completed: false, title: "Купить хлеб" },
-    { id: 2, completed: false, title: "Купить масло" },
-    { id: 3, completed: false, title: "Купить молоко" },
-  ]);
+  const [todos, setTodos] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos?_limit=5")
+      .then((response) => response.json())
+      .then((todos) => {
+        setTimeout(() => {
+          setTodos(todos);
+          setLoading(false);
+        }, 2000);
+      });
+  }, []);
 
   function toggleTodo(id) {
     // console.log("todo id ", id);
@@ -36,9 +45,10 @@ function App() {
         <h1>React tutorial</h1>
         <AddTodo onCreate={addTodo} />
 
+        {loading && <Loader />}
         {todos.length ? (
           <TodoList todos={todos} onToggle={toggleTodo} />
-        ) : (
+        ) : loading ? null : (
           <p>No todos</p>
         )}
       </div>
