124c5fbb90987dc4e289cb68389efc59acd82241
Oleg Kitaev <kitaev_oleg@mail.ru>
2024-06-29 10:08:53 +0300

    add form for adding new todos

---
+++ src/Todo/AddTodo.js
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import PropTypes from "prop-types";
+
+function AddTodo({ onCreate }) {
+  const [value, setValue] = useState("");
+
+  function submitHandler(e) {
+    e.preventDefault();
+
+    if (value.trim()) {
+      onCreate(value);
+      setValue("");
+    }
+  }
+  return (
+    <form style={{ marginBottom: "1rem" }} onSubmit={submitHandler}>
+      <input value={value} onChange={(e) => setValue(e.target.value)} />
+      <button type="submit">Add todo</button>
+    </form>
+  );
+}
+
+AddTodo.propTypes = {
+  onCreate: PropTypes.func.isRequired,
+};
+
+export default AddTodo;
